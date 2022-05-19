b8cff2f820d1d5a30be50b4f9a901dcaafdda166
張祥賢 <johnny890122>
2022-05-19 17:12:04 +0800

    fix

--- Main_daily.py
+++ Main_daily.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import time
@@ -17,7 +17,7 @@ import subprocess
 import schedule
 
 
-# In[2]:
+# In[ ]:
 
 
 cat_name_checked = ['Docked', 'Arrived', 'Counting', 'QC', 'Labeling',
@@ -109,7 +109,7 @@ team_prod_dict = {
 }
 
 
-# In[3]:
+# In[ ]:
 
 
 class gdoc_information():
@@ -119,7 +119,7 @@ class gdoc_information():
         self.SAMPLE_RANGE_NAME = ""
 
 
-# In[4]:
+# In[ ]:
 
 
 ppl_schema = gdoc_information()
@@ -165,7 +165,7 @@ Score_gdoc.SCOPES = "https://docs.google.com/spreadsheets/d/15BGIJYsV7onztRgBoji
 Score_gdoc.SAMPLE_RANGE_NAME = "Daily Update"
 
 
-# In[5]:
+# In[ ]:
 
 
 # Checkpoint 1: 匯入打卡資料並進行前處理
@@ -206,7 +206,7 @@ def read_punch_file(day, revise_station_name, gs):
     return punch_raw_df
 
 
-# In[6]:
+# In[ ]:
 
 
 # Checkpoint 2: 匯入人力資料並進行前處理
@@ -233,7 +233,7 @@ def read_human_data(gs):
     return name_id_dic, id_name_dic, pda_name_dic, pda_id_dic
 
 
-# In[7]:
+# In[ ]:
 
 
 # Checkpoint 3: 將IB_production新增貼標、收發、印標資料
@@ -315,7 +315,8 @@ def add_data_in_inb(time2, gs, day, month_fullname, inb_pics_file_path_new):
     print_df = get_everyday_print_data(day, gs)
     print_df = print_df[["印標人員", "DATE"]]
     print_df['印標人員'] = print_df['印標人員'].apply(lambda x : str(x).replace("x", "0").replace("X", "0"))
-    print_df['印標人員'] = print_df['印標人員'].astype('float').astype("int").astype("str")
+    print_df['印標人員'] = [str(i).replace("''", "") for i in print_df['印標人員']]
+#     print_df['印標人員'] = print_df['印標人員'].astype('float').astype("int").astype("str")
     print_summary = print_df.merge(ppl_schema_df, left_on='印標人員', right_on='貼標ID', how='left')
     print_summary = print_summary[print_summary['員編'].notnull()]
     print_summary['type'] = 'Print'
@@ -345,7 +346,7 @@ def add_data_in_inb(time2, gs, day, month_fullname, inb_pics_file_path_new):
     
 
 
-# In[8]:
+# In[ ]:
 
 
 # Checkpoint 4: 輸入資料格式統一
@@ -368,7 +369,7 @@ def read_ibs(inb_pics_file_path_new, id_name_dic):
     return inb_pic_df[['name', 'operator', 'type', 'create_time', 'pcs', 'box', 'orders']]
 
 
-# In[54]:
+# In[ ]:
 
 
 # Checkpoint 4-2: OB_production
@@ -407,7 +408,7 @@ def read_obs(id_name_dic, pda_id_dic, gs, day):
     return ob_pic_df[['name', 'operator', 'type', 'create_time', 'pcs', 'box', 'orders']]
 
 
-# In[55]:
+# In[ ]:
 
 
 # Checkpoint 4-3: INV_production
@@ -440,7 +441,7 @@ def read_inv(id_name_dic, gs, day):
     return inv_pic_df[['name', 'operator', 'type', 'create_time', 'pcs', 'box', 'orders']]
 
 
-# In[56]:
+# In[ ]:
 
 
 # Checkpoint 4-4: 將IB_production、OB_production、INV_production資料合併，得到whole_df
@@ -464,7 +465,7 @@ def get_whole_df(ib_df, inv_df, ob_df):
     return whole_df
 
 
-# In[57]:
+# In[ ]:
 
 
 # Checkpoint 5-1: 將whole_df、punch_df合併，得到merge_df
@@ -503,7 +504,7 @@ def get_merge_df(whole_df, punch_df):
     return merge_df
 
 
-# In[58]:
+# In[ ]:
 
 
 # Checkpoint 5-2: 將merge_df依各種工作種類合併(位於calculate_score.py)
@@ -523,7 +524,7 @@ def get_valid_csv(merge_df, cat_name_checked, day):
         cat_df.to_csv('Output/incentive_checked/{}/{}.csv'.format(day, cat), encoding="utf_8_sig")
 
 
-# In[59]:
+# In[ ]:
 
 
 # Checkpoint 6: 計算productivity_agent
@@ -597,7 +598,7 @@ def get_prod_agent_score(cat_name, productivity_varable, whole_df, punch_df, age
     return productivity_table
 
 
-# In[60]:
+# In[ ]:
 
 
 # Checkpoint 7: 計算productivity_TL
@@ -655,7 +656,7 @@ def get_prod_TL_score(productivity_varable, team_prod_dict, whole_df, punch_df, 
         productivity_team_function.to_excel(writer, sheet_name='productivity_team_function', index=False, encoding="utf_8_sig")
 
 
-# In[61]:
+# In[ ]:
 
 
 # Checkpoint 8: 將merge_df進行validation，產出 valid_whole_df
@@ -682,7 +683,7 @@ def get_valid_whole_df(merge_df, day):
     return valid_whole_df
 
 
-# In[62]:
+# In[ ]:
 
 
 def submit_score_to_gsheet(df, gs, day):
@@ -706,7 +707,7 @@ def movefileAndPush():
         print("Error occured :".format(e))
 
 
-# In[63]:
+# In[ ]:
 
 
 def get_everyday_tag_data(day, gs):
@@ -734,7 +735,7 @@ def get_everyday_tag_data(day, gs):
 
 
 
-# In[64]:
+# In[ ]:
 
 
 def get_everyday_print_data(day, gs):
@@ -765,7 +766,7 @@ def get_everyday_print_data(day, gs):
     return print_df
 
 
-# In[65]:
+# In[ ]:
 
 
 def output_foler(month_fullname):
@@ -805,7 +806,7 @@ def tmp_output_folder():
         os.makedirs("tmp_output/whole_df/")
 
 
-# In[72]:
+# In[ ]:
 
 
 def main():
@@ -917,7 +918,7 @@ def main():
     print("="*20+"\n")
 
 
-# In[73]:
+# In[ ]:
 
 
 if __name__ == "__main__":
